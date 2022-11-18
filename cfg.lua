ff1a53a16ee213e7c72be99c983da4deb77e7577
Testarossa <austin.w.burrows@gmail.com>
2022-11-18 14:31:41 -0500

    Update cfg.lua

--- cfg.lua
+++ cfg.lua
@@ -1,20 +1,18 @@
 cfg = {}
---This is where you put your DiscordAppID you can get it from here https://discord.com/developers/applications
 cfg.DiscordAppID = 653188008882864148 
 
-cfg.discordImageName = 'monke'
-cfg.hoverText = 'Monke'
+cfg.discordImageName = 'test'
+cfg.hoverText = 'Testing'
 cfg.richPresenceText = "" ..GetPlayerServerId(PlayerId()).. " | " .. #GetActivePlayers().. " Players"
-cfg.smallDiscordImageName = 'monke'
-cfg.smallHoverText = 'Monke'
+cfg.smallDiscordImageName = 'test'
+cfg.smallHoverText = 'Testing'
 
 cfg.button1 = {
-    text = 'Help a Monke!!!',
-    url = 'https://www.youtube.com/watch?v=2OCeMSMACEo'
+    text = 'Testing',
+    url = ''
 }
 
 cfg.button2 = {
-    text = 'How to become monke',
-    url = 'https://www.youtube.com/watch?v=7jE55zkCPI0'
+    text = 'test',
+    url = ''
 }
--- Just to clarify something, you can only have 2 buttons
