6dfeb17c734277365c35ee99b9e3f048abf253e9
Testarossa <austin.w.burrows@gmail.com>
2022-11-18 14:59:53 -0500

    Update cfg.lua

--- cfg.lua
+++ cfg.lua
@@ -3,16 +3,16 @@ cfg.DiscordAppID = 653188008882864148
 
 cfg.discordImageName = 'test'
 cfg.hoverText = 'Testing'
-cfg.richPresenceText = "" ..GetPlayerServerId(PlayerId()).. " | " .. #GetActivePlayers().. " Players"
 cfg.smallDiscordImageName = 'test'
 cfg.smallHoverText = 'Testing'
 
-cfg.button1 = {
-    text = 'Testing',
-    url = ''
-}
+cfg.Link1Toggle = true
+cfg.Link1Button = "testing"
+cfg.Link1 = "https://discord.gg/"
 
-cfg.button2 = {
-    text = 'test',
-    url = ''
-}
+cfg.Link2Toggle = true
+cfg.Link2Button = "testing"
+cfg.Link2 = "https://discord.gg/"
+
+
+cfg.MaxPlayers = '64'
