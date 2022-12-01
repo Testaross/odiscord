9bef03699d59f10d0bf9eef9e74c8f4983d3b62a
Testarossa <austin.w.burrows@gmail.com>
2022-12-01 02:31:11 -0500

    Create shared.lua

---
+++ shared.lua
@@ -0,0 +1,26 @@
+cfg = {}
+cfg.DiscordAppID = 653188008882864148 
+
+
+cfg.discordImageName = 'test'
+cfg.hovertext = 'Test'
+
+cfg.smallDiscordImageName = 'test'
+cfg.smallHoverText = 'Testing'
+
+cfg.Link1Toggle = true
+cfg.Link1Button = "First Button"
+cfg.Link1 = "https://discord.gg/"
+
+cfg.Link2Toggle = true
+cfg.Link2Button = "Second Button"
+cfg.Link2 = "https://discord.gg/"
+
+cfg.NotificationTitle = "Discord Presence"
+cfg.NotificationSuccess = "Discord Presence is now active!"
+cfg.NotificationError = "Discord Presence is not active!"
+cfg.MaxPlayers = '64'
+
+
+cfg.toggle = true
+cfg.togglecommand = 'discordtoggle'
